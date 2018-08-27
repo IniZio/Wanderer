cb9293979f7ed80256be18c0af2af6e7dc339405
tmtmtmtam <tmtam@connect.ust.hk>
2018-08-27 17:41:58 +0800

    setup PlayerObjprefab

---
+++ Assets/PlayerObj.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObj : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("onStart");
+        Instantiate(PlayerUnitPrefab);
+	}
+
+    public GameObject PlayerUnitPrefab;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
