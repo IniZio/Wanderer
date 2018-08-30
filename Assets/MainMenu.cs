5383b99ff250e367b1b544f408274ec4b3b1a7dd
tmtmtmtam <tmtam@connect.ust.hk>
2018-08-30 14:13:28 +0800

    button script

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public Button NewGameButton;
+
+    public Button LoadGameButton;
+
+    public Button JoinGameButton;
+
+    public Button QuitButton;
+
+    public string NewGameSceneName;
+
+    public GameObject LoadGameMenu;
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(NewGameSceneName);
+    }
+
+    public void LoadGame()
+    {
+        LoadGameMenu.SetActive(true);
+    }
+
+    public void JoinGame()
+    {
+
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
