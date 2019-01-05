8640fa60a1bddeabeaf46bca4f47619ff68fbc27
inizio <newman.kcchow@gmail.com>
2019-01-06 04:51:23 +0800

    make animal escape from player

---
+++ Assets/BasicMovement1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement1 : MonoBehaviour
+{
+    private CharacterController _controller;
+
+    public int Speed;
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        move.y -= Physics.gravity.magnitude * Time.deltaTime;
+        _controller.Move(move * Time.deltaTime * Speed);
+    }
+
+}
---
+++ Assets/EscapeAnimal.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EscapeAnimal : MonoBehaviour {
+    public float minRange;
+    public int Speed;
+
+    private NavMeshAgent agent;
+    private Vector3 thisPos;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int multiplier = 1;
+        thisPos = transform.position;
+
+        Collider[] collis = Physics.OverlapSphere(transform.position, minRange);
+
+        foreach(Collider colli in collis)
+        {
+            if (colli && colli.tag == "Player")
+            {
+                // transform.position += Vector3.Lerp(thisPos, -colli.transform.position, Time.deltaTime * Speed);
+                Vector3 runTo = transform.position + ((transform.position - colli.transform.position) * multiplier);
+                float distance = Vector3.Distance(transform.position, colli.transform.position);
+                if (distance < minRange) agent.SetDestination(runTo);
+            }
+        }
+    }
+}
