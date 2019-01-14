cd51795fdfadc169719ad8ed7b8b19b1ab449b0f
Tom Tam <31676998+tmtmtmtam@users.noreply.github.com>
2019-01-14 16:58:51 +0800

    dd player camera template

---
+++ Assets/Scripts/PlayerCharacter/PlayerCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FYP.Game.Carmera {
+	public class Charactor : MonoBehaviour {
+		public int speed = 10;
+		// Use this for initialization
+		void Start () {
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			float horizontal = Input.GetAxis("Mouse X") * speed;
+			float vertical = Input.GetAxis("Mouse Y") * speed;
+
+			transform.Rotate(0f, horizontal, 0f, Space.World);
+			transform.Rotate(-vertical, 0f, 0f, Space.Self);
+		}
+	}
+}
+
