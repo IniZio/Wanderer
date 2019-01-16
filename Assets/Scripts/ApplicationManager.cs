9b8e9c8dab005cf9894f731e82e8a923fb4b1427
kplauae <kplauae@connect.ust.hk>
2019-01-16 21:23:58 +0800

    update

--- Assets/Scripts/ApplicationManager.cs
+++ Assets/Scripts/ApplicationManager.cs
@@ -1,12 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using Leap.Unity.Interaction;
 
 public class ApplicationManager : MonoBehaviour {
 	
 
 	public void Quit () 
 	{
-		#if UNITY_EDITOR
+        Debug.Log("Quit");
+#if UNITY_EDITOR
+        Debug.Log("Quit");
 		UnityEditor.EditorApplication.isPlaying = false;
 		#else
 		Application.Quit();
---
+++ Assets/twst.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Leap.Unity.Interaction;
+
+public class twst : MonoBehaviour {
+
+    private InteractionBehaviour _intObj;
+
+    public void Quit() {
+        _intObj = GetComponent<InteractionBehaviour>();
+        Debug.Log("ok");
+
+    }
+
+}
