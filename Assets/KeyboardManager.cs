463e683a390aca19dfa725dc4ad4bc61aa776467
kplauae <kplauae@connect.ust.hk>
2019-01-24 15:04:49 +0800

    keyboard

---
+++ Assets/KeyboardManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Leap.Unity.Interaction;
+
+public class KeyboardManager : MonoBehaviour {
+    
+    string word = null;
+    int wordIndex = 0;
+    string alpha;
+    public Text myName = null;
+
+    public void alphabetFunction(string alphabet){
+
+        wordIndex++;
+        word = word + alphabet;
+        myName.text = word;
+        
+    }
+
+
+}
