c72456d84cc7f515c0dc9f05b432b14b30b6f303
kplauae <kplauae@connect.ust.hk>
2019-01-24 16:19:32 +0800

    keyboard

--- Assets/KeyboardManager.cs
+++ Assets/KeyboardManager.cs
@@ -10,12 +10,14 @@ public class KeyboardManager : MonoBehaviour {
     int wordIndex = 0;
     string alpha;
     public Text myName = null;
+    public TextMesh Name = null;
 
     public void alphabetFunction(string alphabet){
 
         wordIndex++;
         word = word + alphabet;
         myName.text = word;
+        Name.text = word;
         
     }
 
