2852f53162ecf6b08f3b9d4648393ff5eed223f0
Tom Tam <31676998+tmtmtmtam@users.noreply.github.com>
2019-02-01 00:23:59 +0800

    add player prefab to file

---
+++ Assets/Scripts/Save/FileIO.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Fyp.Game.Save {
+	public class FileIO {
+		public static void WriteSave(string path, string content) {
+			StreamWriter writer = new StreamWriter(path);
+			writer.Write(content);
+
+			writer.Close();
+		}
+
+		public static void ReadSave(string path) {
+			try {
+				StreamReader reader = new StreamReader(path);
+				string content = reader.ReadToEnd();
+				reader.Close();
+				Debug.Log(content);
+			}
+			catch (System.Exception err) {
+				Debug.Log("Read file error: " + err.ToString());
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Save/GameSave.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fyp.Game.Save {
+	public class GameSave : ScriptableObject  {
+		#region Singleton Code
+		protected static GameSave sInstance = null;
+
+		public static GameSave Instance {
+			get {
+				if(sInstance == null) {
+					sInstance = ScriptableObject.CreateInstance<GameSave>();
+					sInstance.hideFlags = HideFlags.HideAndDontSave;
+				}
+				return sInstance;
+			}
+		}
+		#endregion
+
+		public string mockPath = "/test.fyp";
+
+		void SaveTest() {
+			string path = Application.persistentDataPath + mockPath;
+			FileIO.WriteSave(path, "testing");
+		}
+	}
+}
