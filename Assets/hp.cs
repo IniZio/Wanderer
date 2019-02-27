2d59f5b55dd7589b70e338e6f98151f5d081d84d
inizio <newman.kcchow@gmail.com>
2019-02-27 18:51:42 +0800

    adds health bar to main menu
    - simple bar
    - HP +/- buttons
    - hp update script

---
+++ Assets/hp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hp : MonoBehaviour {
+    public SimpleHealthBar healthBar;
+
+    private int health = 100;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	public void Increase () {
+        health += 10;
+        healthBar.UpdateBar(health, 100);
+    }
+
+    public void Decrease() {
+        health -= 10;
+        healthBar.UpdateBar(health, 100);
+    }
+}
