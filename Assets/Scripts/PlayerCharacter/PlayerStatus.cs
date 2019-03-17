22c5bd58f319e2b3a9940fae4bb6f9e3f8145df4
Tom Tam <31676998+tmtmtmtam@users.noreply.github.com>
2019-03-17 21:40:55 +0800

    waiting room door

---
+++ Assets/Scripts/PlayerCharacter/PlayerStatus.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.Serialization;
+
+namespace Fyp.Game.PlayerControl {
+	public class PlayerStatus {
+		public bool readyForPlay = false;
+		public bool isMaster;
+
+		public PlayerStatus(bool isMaster) {
+			this.isMaster = isMaster;
+		}
+
+		public PlayerStatus() {
+			this.isMaster = false;
+		}
+
+		public void setIsMaster(bool isMaster) {
+			this.isMaster = isMaster;
+		}
+
+		public void ReadyToPlay() {
+			this.readyForPlay = !this.readyForPlay;
+		}
+		public void setIsReady(bool isReady) {
+			this.readyForPlay = isReady;
+		}
+
+		public bool isReady() {
+			return this.readyForPlay;
+		}
+
+		public bool isMasterPlayer() {
+			return this.isMaster;
+		}
+	}
+}
