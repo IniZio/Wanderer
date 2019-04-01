1bfdfce1c5e6c9aeb1eccf46aa0bc44f12ab474f
Tom Tam <31676998+tmtmtmtam@users.noreply.github.com>
2019-04-01 16:21:45 +0800

    re do scene leap rip

--- Assets/Scripts/PlayerCharacter/PlayerCamera.cs
+++ Assets/Scripts/PlayerCharacter/PlayerCamera.cs
@@ -4,53 +4,17 @@ using UnityEngine;
 
 namespace Fyp.Game.Carmera {
 	public class PlayerCamera : MonoBehaviour {
-		// public int speed = 10;
-
-		// public Camera firstPersonViewCamera;
-		// public Camera menuViewCamera;
-
-		// public bool onMenuView;
-
-		// // Use this for initialization
-		// void Start () {
-		// 	firstPersonViewCamera.enabled = false;
-		// 	menuViewCamera.enabled = true;
-		// 	onMenuView = true;
-		// }
-
-		// // Update is called once per frame
-		// void Update()
-		// {
-		// 	float horizontal = Input.GetAxis("Mouse X") * speed;
-		// 	float vertical = Input.GetAxis("Mouse Y") * speed;
-
-		// 	transform.Rotate(0f, horizontal, 0f, Space.World);
-		// 	transform.Rotate(-vertical, 0f, 0f, Space.Self);
-		// }
-
-		// void switchCamera() {
-		// 	firstPersonViewCamera.enabled = !firstPersonViewCamera.enabled;
-		// 	menuViewCamera.enabled = !menuViewCamera.enabled;
-		// 	onMenuView = !onMenuView;
-		// }
-
 		public GameObject player;
-		private Vector3 offset;
-		private Vector3 playerHeight = new Vector3((float) 0.2, (float) 0, (float) 0);
-
-		void start() {
-			offset = transform.position;
-		}
+		private Vector3 playerHeight = new Vector3((float) 0, (float) 0, (float) 0);
 
 		void LateUpdate() {
 			if (this.player != null) {
-				transform.position = player.transform.position + offset + playerHeight;
-            	transform.rotation = player.transform.rotation;
+				transform.rotation = player.transform.rotation;
+				transform.position = player.transform.position + playerHeight;
+				transform.position = transform.position + transform.forward * (float)0.15;
 			}
 		}
-
 		public void setCamera(GameObject player) {
-			Debug.Log("set Camera");
 			this.player = player;
 		}
 	}
