c5a79bab8a08e324e7d376d90b074dccaf947b46
Tom Tam <31676998+tmtmtmtam@users.noreply.github.com>
2019-04-14 16:55:21 +0800

    panel scirpt

--- Assets/Scripts/Constant/GameConstant.cs
+++ Assets/Scripts/Constant/GameConstant.cs
@@ -3,16 +3,20 @@ namespace Fyp.Constant{
         public static readonly string GAME_VERSION = "1.0.0";
         public static readonly string GAME_NAME = "My Game~~";
 
-        public static class ScenceName{
+        public static class ScenceName {
             public static string MAIN_MENU { get { return "MainMenu"; } }
             public static string WAITING_ROOM { get { return "WaitingRoom"; } }
         }
 
-        public static class ToolList{
+        public static class ToolList {
 
         }
 
-        public static class WeaponList{
+        public static class WeaponList {
+
+        }
+
+        public static class UpgradeList {
 
         }
     }
---
+++ Assets/Scripts/Scenes/UpgradePanel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Fyp.Game.UI {
+    public class UpgradePanel : MonoBehaviour {
+        public GameObject targetBox;
+        public GameObject sourceBoxWood, sourceBoxStone, sourceBoxIron;
+        void selectTools(string typeName) {
+
+        }
+    }
+}
