c5c3fee1350da6abe40de93e1c9ed15fe103fbef
Tom Tam <31676998+tmtmtmtam@users.noreply.github.com>
2019-05-02 21:20:15 +0800

    adjust spawn point

--- Assets/Scripts/Scenes/RandomMapManager.cs
+++ Assets/Scripts/Scenes/RandomMapManager.cs
@@ -9,10 +9,13 @@ namespace Fyp.Game.UI {
         public RandomMap.GameManager maze;
         public GameObject Player1, Player2, P1point, P2point, FollowCamera;
         public ControlScript P1Script, P2Script;
+        public bool firstMap = false;
 
         void Awake() {
         }
         void Start() {
+            this.MapPlayer1();
+            this.MapPlayer2();
         }
 
         void Update() {
@@ -26,13 +29,15 @@ namespace Fyp.Game.UI {
                 Random.InitState(P1Script.randomSeed);
                 maze.BeginGame();
             }
+            if (this.Player1 && this.Player2 && this.maze.isGen && !this.firstMap) {
+                TransportToSpawnPoint();
+            }
         }
         void MapPlayer1() {
             GameObject Player1 = GameObject.FindWithTag("Player1Character");
             if (Player1 != null) {
                 this.Player1 = Player1;
                 P1point = GameObject.FindWithTag("Player1SpawnPoint");
-                this.Player1.transform.position = P1point.transform.position;
                 ControlScript script = Player1.GetComponent("ControlScript") as ControlScript;
                 if(script.getIsMe()) {
                     PlayerCamera cameraScirpt = FollowCamera.GetComponent("PlayerCamera") as PlayerCamera;
@@ -46,7 +51,6 @@ namespace Fyp.Game.UI {
             if (Player2 != null) {
                 this.Player2 = Player2;
                 P2point = GameObject.FindWithTag("Player2SpawnPoint");
-                this.Player2.transform.position = P2point.transform.position;
                 ControlScript script = Player2.GetComponent("ControlScript") as ControlScript;
                 if(script.getIsMe()) {
                     PlayerCamera cameraScirpt = FollowCamera.GetComponent("PlayerCamera") as PlayerCamera;
@@ -54,6 +58,12 @@ namespace Fyp.Game.UI {
                 }
                 this.P2Script = script;
             }
+        }
+
+        void TransportToSpawnPoint() {
+            this.firstMap = true;
+            this.Player2.transform.position = P2point.transform.position;
+            this.Player1.transform.position = P1point.transform.position;
         }
     }
 }
