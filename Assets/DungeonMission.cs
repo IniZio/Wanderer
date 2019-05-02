2d831b0d65a389a2052d67fcf030402012bad80d
inizio <newman.kcchow@gmail.com>
2019-05-02 19:34:58 +0800

    fail mission transition

--- Assets/DungeonMission.cs
+++ Assets/DungeonMission.cs
@@ -16,7 +16,8 @@ public class DungeonMission : Photon.PunBehaviour
 
     private NPCManager npcManager;
     private Mission2 mission2;
-    private float alphaFadeValue;
+    private readonly float alphaFadeValue;
+    private bool justFailed = false;
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
@@ -24,11 +25,13 @@ public class DungeonMission : Photon.PunBehaviour
         {
             stream.SendNext(nextMission);
             stream.SendNext(mission2);
+            stream.SendNext(justFailed);
         }
         else
         {
             nextMission = (Constants.Mission)stream.ReceiveNext();
             mission2 = (Mission2)stream.ReceiveNext();
+            justFailed = (bool)stream.ReceiveNext();
         }
     }
 
@@ -41,6 +44,12 @@ public class DungeonMission : Photon.PunBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (justFailed)
+        {
+            justFailed = false;
+            StartCoroutine(_FailMission());
+        }
+
         if (PhotonNetwork.isMasterClient)
         {
             switch (nextMission)
@@ -106,13 +115,21 @@ public class DungeonMission : Photon.PunBehaviour
 
     public void FailMission()
     {
+        justFailed = true;
+    }
+
+    IEnumerator _FailMission()
+    {
         UnityEngine.UI.Image blackScreen = GameObject.Find("Death").GetComponent<UnityEngine.UI.Image>();
 
         blackScreen.color = Color.black;
         blackScreen.canvasRenderer.SetAlpha(0.0f);
         blackScreen.CrossFadeAlpha(1.0f, 3, false);
 
+        yield return new WaitForSeconds(3);
+
         NetworkChangeScene.AllPlayerChangeScene("BaseNew");
+        yield break;
     }
 
     public void FinishMission(Constants.Mission mission)
