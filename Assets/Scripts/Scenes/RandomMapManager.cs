5c8a7854f70e2292033f0a3ed6b9d5806e02145d
Tom Tam <31676998+tmtmtmtam@users.noreply.github.com>
2019-05-02 19:16:13 +0800

    add playerview and spawn point on random map

--- Assets/Scripts/Scenes/RandomMapManager.cs
+++ Assets/Scripts/Scenes/RandomMapManager.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Fyp.Game.PlayerControl;
+using Fyp.Game.Carmera;
 
 namespace Fyp.Game.UI {
     public class RandomMapManager : MonoBehaviour {
         public RandomMap.GameManager maze;
-        public GameObject Player1, Player2;
+        public GameObject Player1, Player2, P1point, P2point, FollowCamera;
         public ControlScript P1Script, P2Script;
 
         void Awake() {
@@ -32,7 +33,13 @@ namespace Fyp.Game.UI {
             GameObject Player1 = GameObject.FindWithTag("Player1Character");
             if (Player1 != null) {
                 this.Player1 = Player1;
+                P1point = GameObject.FindWithTag("Player1SpawnPoint");
+                this.Player1.transform.position = P1point.transform.position;
                 ControlScript script = Player1.GetComponent("ControlScript") as ControlScript;
+                if(script.getIsMe()) {
+                    PlayerCamera cameraScirpt = FollowCamera.GetComponent("PlayerCamera") as PlayerCamera;
+                    cameraScirpt.setCamera(this.Player1);
+                }
                 this.P1Script = script;
             }
         }
@@ -40,7 +47,12 @@ namespace Fyp.Game.UI {
             GameObject Player2 = GameObject.FindWithTag("Player2Character");
             if (Player2 != null) {
                 this.Player2 = Player2;
+                this.Player2.transform.position = P2point.transform.position;
                 ControlScript script = Player2.GetComponent("ControlScript") as ControlScript;
+                if(script.getIsMe()) {
+                    PlayerCamera cameraScirpt = FollowCamera.GetComponent("PlayerCamera") as PlayerCamera;
+                    cameraScirpt.setCamera(this.Player2);
+                }
                 this.P2Script = script;
             }
         }
