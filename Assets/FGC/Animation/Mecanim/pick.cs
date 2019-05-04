4ae5dd47b313ce472ade4169eee158331d4a3ff6
Tom Tam <31676998+tmtmtmtam@users.noreply.github.com>
2019-05-04 17:49:07 +0800

    add new animator

---
+++ Assets/FGC/Animation/Mecanim/pick.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pick : MonoBehaviour {
+
+
+	void Start () {
+		
+	}
+
+	void Update () {
+
+	}
+	void OnTriggerEnter(Collider col){
+		var tag=col.gameObject.tag;
+		Debug.Log("col");
+		 if(tag=="Player"){
+			PlayerControl script=col.gameObject.GetComponent("PlayerControl") as PlayerControl;
+					Debug.Log("player");
+			script.pick(this.gameObject);
+		 }
+	}
+
+	void OnCollisionExit(Collision collision){}
+
+	void OnCollisionStay(Collision collision){}
+}
